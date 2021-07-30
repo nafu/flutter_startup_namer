b89b46badabedc4eac127d42f2391a4179d47904
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2021-07-30 19:48:39 +0900

    Add some variables

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,8 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
